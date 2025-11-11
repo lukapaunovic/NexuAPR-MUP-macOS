35d275ac9e9953211d86807d01ec7e52c9afedce
Luka Paunović <internetfazoni@gmail.com>
2025-11-12 00:28:54 +0100

    Update build_pkg.sh

--- build_pkg.sh
+++ build_pkg.sh
@@ -5,7 +5,7 @@ echo "Kreiram PKG koristeći postojeće NexuAPR.app i scripts..."
 pkgbuild \
   --root "$HOME/nexu_pkg_build/root" \
   --identifier com.nexu.apr.mup \
-  --version 1.1 \
+  --version 1.2 \
   --install-location "/Applications" \
   --scripts "$HOME/nexu_pkg_build/scripts" \
   "$HOME/Desktop/NexuAPR_MUP.pkg"
