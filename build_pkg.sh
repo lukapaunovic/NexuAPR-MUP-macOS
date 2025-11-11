7c9862def072595020d495e6d3bb137258374cdf
Luka <luka@Lukas-MacBook-Pro.local>
2025-11-11 18:36:54 +0100

    Initial commit

---
+++ build_pkg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Kreiram PKG koristeći postojeće NexuAPR.app i scripts..."
+
+pkgbuild \
+  --root "$HOME/nexu_pkg_build/root" \
+  --identifier com.nexu.apr.mup \
+  --version 1.1 \
+  --install-location "/" \
+  --scripts "$HOME/nexu_pkg_build/scripts" \
+  "$HOME/Desktop/NexuAPR_MUP.pkg"
+
+echo "Gotovo! PKG je na Desktop-u."
